faadf9580a26bd3f52455ec7069c5c85326825d6
salwasuh <salwakurnia10@gmail.com>
2023-09-21 11:45:43 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello world!</h>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio vero temporibus in dolor nesciunt blanditiis sapiente, accusamus quia dolorem voluptatibus, explicabo rem quam sint laboriosam rerum ab, ratione repellendus eligendi.</p>
 </body>
 </html>
